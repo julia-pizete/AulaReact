cd77283c273c2b89675187bf6f8c6c19dae08950
julia-pizete <pizetejulia@gmail.com>
2026-08-11 10:22:16 -0300

    11/8

--- aula03/src/App.jsx
+++ aula03/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from"react-router-dom";
 
 import Home from "./paginas/Home";
 import SaoPaulo from "./paginas/SaoPaulo";
@@ -6,21 +6,28 @@ import MinasGerais from "./paginas/MinasGerais";
 import RioJaneiro from "./paginas/RioJaneiro";
 import EspiritoSanto from "./paginas/EspiritoSanto";
 
+
 export default function App()
 {
-  return (
-    <BrowserRouter>
-     <Routes>
-       <Route path="/" element={<Home/>}/>
+return(
+<BrowserRouter>
 
-       <Route path="/estados/sp" element={<SaoPaulo />}/>
-       <Route path="/estados/mg" element={<MinasGerais />}/>
-       <Route path="/estados/rj" element={<RioJaneiro />}/>
-       <Route path="/estados/es" element={<EspiritoSanto />}/>
+<Routes>
 
-       
-     </Routes>
-    
-    </BrowserRouter>
-  );
+<Route path="/" element={<Home />} />
+
+<Route path="/estados/sp" element={<SaoPaulo />}  />
+<Route path="/estados/mg" element={<MinasGerais />}  />
+<Route path="/estados/rj" element={<RioJaneiro />}  />
+<Route path="/estados/es" element={<EspiritoSanto />}  />
+
+</Routes>
+
+</BrowserRouter>
+
+
+
+);
+
+
 }
